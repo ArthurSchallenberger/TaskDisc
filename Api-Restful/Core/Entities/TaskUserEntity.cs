e7156a1773279f2fd004f7f70836dea0948dc7e6
ArthurSchallenberger <arthurscha7788@gmail.com>
2025-06-24 20:55:23 -0300

    feat: new version of database (add Id on TaskUser table)

--- Api-Restful/Core/Entities/TaskUserEntity.cs
+++ Api-Restful/Core/Entities/TaskUserEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace Api_Restful.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_Restful.Core.Entities;
 public class TaskUserEntity
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     public int Id_User { get; set; }
     public int Id_Task { get; set; }
 
